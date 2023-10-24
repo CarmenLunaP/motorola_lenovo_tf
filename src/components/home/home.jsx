b4ce86e71a83964f08232dc4b16f33fb6c6e1743
Carmen Luna <lunacarmen22@gmail.com>
2023-10-24 17:22:14 -0500

    carmenlunap create home page

---
+++ src/components/home/home.jsx
@@ -0,0 +1,16 @@
+import SearchBar from "../commonComponents/searchBar/searchBar";
+import "./home.css";
+import React from "react";
+
+function Home () {
+
+    return (
+        <div>
+          <SearchBar />
+          <h1>HOLA HOME</h1>
+         
+        </div>
+      );
+    }
+
+    export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./components/login/login.jsx"
+import Home from './components/home/home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Login />} /> 
+        <Route path="/home" element={<Home />} /> 
         
       </Routes>
     </Router>
