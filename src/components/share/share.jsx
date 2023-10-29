a0b17c5f9fd470f915b99ff2d1e3b63d5a00a93e
Fabiana Hasu <fabi.hasu@gmail.com>
2023-10-29 15:36:52 -0300

    btn volver vista share

--- src/components/share/share.jsx
+++ src/components/share/share.jsx
@@ -6,22 +6,43 @@ import { Button } from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
 import WhatsApp from "@mui/icons-material/WhatsApp";
 import MailOutlined from "@mui/icons-material/MailOutlined";
+import { Link } from "react-router-dom";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+
 
 function Share() {
   return (
     <div className="share-content-container">
       <SideBar />
       <div className="compartir">
+        
         <SearchBar />
         <div className="fondo">
           <div className="msj-compartir">
             <p>Compartir</p>
+            <div className="btn-volver-container">
+              <Link to="/searchResult.jsx">
+                <div className="share-volver-content">
+                  <span>
+                    {" "}
+                    <ArrowBackIosIcon /> Volver{" "}
+                  </span>
+                </div>
+              </Link>
+            </div>
           </div>
+
           <div className="mensaje-final">
             <p>
-              Hemos recibido su Motorola E20 para servicio técnico o garantía,
-              nos complace informarle que hemos decidido reemplazarlo por un
-              nuevo Motorola G31 con notables mejoras:
+              ¡Estás a punto de disfrutar de un rendimiento excepcional con el
+              nuevo Moto G31! Esta actualización te ofrece una memoria RAM que
+              varía de 2GB a 6GB, lo que significa un aumento significativo en
+              la velocidad y la capacidad de multitarea. La memoria ROM se ha
+              ampliado de 32GB a 128GB, lo que te brinda más espacio para tus
+              aplicaciones y archivos. Además, la cámara principal se ha
+              mejorado, pasando de 13MP a 64MP, lo que te permitirá capturar
+              fotos más nítidas y detalladas. ¡Es un salto impresionante en
+              rendimiento y capacidad!
             </p>
           </div>
           <div className="mensaje-final">
