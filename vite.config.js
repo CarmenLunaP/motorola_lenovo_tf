8bd646954889b4ea5cda94d819527c4af9701a11
Carmen Luna <lunacarmen22@gmail.com>
2023-10-30 09:00:25 -0500

    fix deployment 8

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,5 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig(({ command }) => {
-  if (command === 'build') {
-    return {
-      root: 'src',
-      build: {
-        minify: false,
-        rollupOptions: {
-          input: 'main.jsx', 
-          output: {
-            dir: './dist',
-          },
-        },
-      },
-    };
-  }
-  return {};
-});
-
-
-// export default {
-//   server: {
-//     port: 3000, 
-//   },
-// };
+export default {
+  server: {
+    port: 3000, 
+  },
+};
